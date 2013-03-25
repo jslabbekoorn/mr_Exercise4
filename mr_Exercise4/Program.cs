c2eea82b840181acf9fd4656fd01583369d5f91b
John Slabbekoorn <john.slabbekoorn@gmail.com>
2013-03-25 21:53:50 +0100

    New

---
+++ mr_Exercise4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mr_Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intGetal;
+            int intInput;
+
+            Console.WriteLine("Kies een getal tussen de 0 en 9");
+
+            do
+            {
+                Random randNummer = new Random();
+                intGetal = randNummer.Next(10);
+
+                intInput = Int16.Parse(Console.ReadLine());
+
+                if (intGetal != intInput)
+                {
+                    Console.WriteLine("Helaas, ik had " + intGetal + " in gedachten");
+                }
+            }
+            while (intGetal != intInput);
+
+            Console.WriteLine("Correct!");
+            Console.ReadLine();
+        }
+    }
+}
